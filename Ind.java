7ed5a07838d9e7988e1dc1fbf0edbed8ae64c0ff
ANSHU BHARDWAJ <anshubhardwaj9876@gmail.com>
2025-04-18 11:37:00 +0530

    Add files via upload

---
+++ Ind.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ind {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.print("Enter the element to find: ");
+        int target = scanner.nextInt();
+        int index = Ind(arr, target);
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index: " + index);
+        } else {
+            System.out.println("Element " + target + " not found in the array.");
+        }
+
+        scanner.close();
+    }
+    public static int Ind(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
